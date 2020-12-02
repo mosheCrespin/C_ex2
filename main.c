ccb2a5b0b4be8ac50bd78d21d324ca70e75ae7b4
Moshe <moshec315@gmail.com>
2020-12-03 00:52:21 +0200

    check

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 int main()
 {
     int flag=FALSE;
+    //run until the user init E 
     while (flag==FALSE)
     {
     printf("\nPlease choose a transaction type:\n O-Open Account\n B-Balance Inquiry\n D-Deposit\n W-Withdrawal\n C-Close Account\n I-Interest\n P-Print\n E-Exit\n");
@@ -12,106 +13,107 @@ int main()
     scanf(" %c" ,&input);
     switch (input)
     {
-    case 'O':{
+    case 'O':{//if the input is o get in.
         double deposit;
         double q;
         q=getNumberOfOpenAccounts();
-        if(q<49){
+        if(q<49){//check if there is less then 50 acounts 
         printf("Please enter amount for deposit: ");
-        if(scanf(" %lf" ,&deposit)){//check if the input is a number
+        if(scanf(" %lf" ,&deposit)){//check if the input is a number.
           if(deposit>=0){//deposit can't be negative 
-          deposit=((double)((int)(deposit*100))/100);
+          deposit=((double)((int)(deposit*100))/100);//take deposit number until 2 numbers after the point.
           	O(deposit);
           }
-          else printf("Invalid Amount\n");
+          else printf("Invalid Amount\n");//if deposite is negative number.
         }
         else{
-          printf("Failed to read the amount\n");
+          printf("Failed to read the amount\n");//if if the input isn't a number.
         }
      }
-     else printf("there is no place for new account\n");
+     else printf("there is no place for new account\n");//if there is 50 acounts
      break;
     }
-      case 'B': {
+      case 'B': {//if the input is B get in.
         int account_number;
         printf("Please enter account number: ");
-        if(scanf(" %d",&account_number))
+        if(scanf(" %d",&account_number))//check if the new input is a number. 
         {
-          if(isInRange(account_number)){//check if account number is in range
-          	if(isopen(account_number))
+          if(isInRange(account_number)){//check if account number is in range.
+          	if(isopen(account_number))//if the acount is open.
             		B(account_number);
-            	else printf("This account is closed\n");	
+            	else printf("This account is closed\n");//if the acount isn't already open.	
           }
         }
-        else{
+        else{//if the input isn't a number
           printf("Failed to read the account number\n");
         }
         break;
 
         }
-     case 'D' :{
+     case 'D' :{//if the input is D get in.
        int account_number;
         printf("Please enter account number: ");
-        if(scanf(" %d",&account_number))
+        if(scanf(" %d",&account_number))//check if the new input is a number.
         {
          if(isInRange(account_number)){//check if account number is in range
             if(isopen(account_number)){//check if the account is open
               printf("Please enter the amount to deposit: ");
               double deposit;
-              if(scanf(" %lf", &deposit)){
-                 deposit=((double)((int)(deposit*100))/100);
+              if(scanf(" %lf", &deposit)){//check if the new deposite is number.
+                 deposit=((double)((int)(deposit*100))/100);//take the deposite until 2 numbers after the point.
                  if(deposit>=0)
                      D(account_number,deposit);
-                     else printf("Cannot deposit a negative amount\n");
+                     else printf("Cannot deposit a negative amount\n");//if negative.
               }
-              else
+              else//if the deposite isn't a number.
               {
                 printf("Failed to read the amount\n");
               }
               
           }
-            else printf("This account is closed\n");	
+            else printf("This account is closed\n");	//if the acount is close.
         }
-          else printf("Invalid account number\n");
+          else printf("Invalid account number\n");//if the acount number isn't in the range.
         }
-        else{
+        else{//if the input isn't a number.
         printf("Failed to read the account number\n");
         }
         break;
      }
-      case 'W':{
+      case 'W':{//if the input is W get in.
         int account_number;
         printf("Please enter account number: ");
-        if(scanf(" %d",&account_number))
+        if(scanf(" %d",&account_number))//if the new input is a number get in.
         {
           if(isInRange(account_number)){//check if account number is in range
             if(isopen(account_number)){//check if the account is open
               double withdrawal;
               printf("Please enter the amount to withdraw: ");
-              if(scanf(" %lf", &withdrawal)){
-              withdrawal=((double)((int)(withdrawal*100))/100);
+              if(scanf(" %lf", &withdrawal)){//cheak if the input is number
+              withdrawal=((double)((int)(withdrawal*100))/100);//take until 2 number after the point.
               			W(account_number,withdrawal);
               }
-              else
+              else//if the input isn't a number.
               {
                 printf("faild to read withdrow\n");
               }
               
-          }
+          }//if the acount isn't open.
           else printf("This account is closed\n");	
+        }//TODO
+         else printf("Invalid account number\n");//if the acount number isn't in the range.
         }
-        }
-        else{
+        else{//if the account_number isn't number.
            printf("Failed to read the account number\n");
         }
 
       break;
     
       }
-     case 'C':{
+     case 'C':{//if the input is C get in.
         int account_number;
         printf("Please enter account number: ");
-        if(scanf(" %d",&account_number))
+        if(scanf(" %d",&account_number))//if the new input is number.
         {
           if(isInRange(account_number)){//check if account number is in range
             if(isopen(account_number)){//check if the account is open
@@ -119,38 +121,39 @@ int main()
              printf("Closed account number %d\n", account_number);
              
           }
-            	else printf("This account is already closed\n");
-            	}	
+            	else printf("This account is already closed\n");//if the acount isn't already open.
+            	}//todo
+            	 else printf("Invalid account number\n");//if the acount number isn't in the range.	
           }
-        else{
+        else{//if the new input isn't a number.
         printf("Failed to read the account number\n");
     }
     break;
      }
-      case 'I':{
+      case 'I':{//if the input is I get in.
         int interest;
         printf("Please enter interest rate: ");
-        if(scanf(" %d",&interest)==1){
+        if(scanf(" %d",&interest)==1){//check if the net input is number.
 		 if(interest>=0)//interest can't negative    	
           		I(interest);
           	else printf("Invalid interest rate\n");		
         }
-        else
+        else//the new input isn't a number.
         {
          printf("Failed to read the interest rate\n");
         }
           break;
     }
-      case 'P':{
+      case 'P':{//if the input is P get in.
            P();
            break;
     }
-    case 'E':{
+    case 'E':{//if the input is E.
       E();
-      flag=TRUE;
+      flag=TRUE;//this line break thw while.
       break;
     }
-    default:
+    default://else
         printf("Invalid transaction type\n");
     }
     }
--- myBank.c
+++ myBank.c
@@ -6,68 +6,70 @@
 #define END 950
 double matrix_2d [ACCOUNTS][SITUATIONS]={0};
 int i=0;
-void O(double deposit){
+void O(double deposit){//this function is to open a new acount.
     int j=0;
-    while(matrix_2d[j][1]==1){j++;}
-    matrix_2d[j][0]=deposit;
-    matrix_2d[j][1]=1;
+    while(matrix_2d[j][1]==1)//run until find acount that is close.
+    {j++;}
+    matrix_2d[j][0]=deposit;//init the deposite in the acount.
+    matrix_2d[j][1]=1;//init this acount to open.
     printf( "New account number is: %d \n", (STARTER+j) );
-    i++;
+    i++;//count the numbers of acounts.
 }
-void B(int account_number){
-        if(matrix_2d[account_number-STARTER][1]==1){
+
+void B(int account_number){//this function is to know the acount balance. 
+        if(matrix_2d[account_number-STARTER][1]==1){//check if this acount is open
         printf( "The balance of account number %d is: %.2lf\n", account_number, matrix_2d[account_number-STARTER][0]);
         }
-        else{
+        else{//if this acount is close.
             printf("This account is closed\n");
         }
     }
-void D (int account_number,double deposit){
-        matrix_2d[account_number-STARTER][0]+=deposit;
+void D (int account_number,double deposit){//this function is to cash deposite in the acount.
+        matrix_2d[account_number-STARTER][0]+=deposit;//add the deposite to the acount.
         printf("The new balance is: %.2lf\n" , matrix_2d[account_number-STARTER][0]);
         }
-void W(int account_number,double withdrawal){
-    if(matrix_2d[account_number-STARTER][0]>=withdrawal){
-        matrix_2d[account_number-STARTER][0]-=withdrawal;
+void W(int account_number,double withdrawal){//this function is to cash withdrawal from the acount.
+    if(matrix_2d[account_number-STARTER][0]>=withdrawal){//if the money the user want to withdrawal is less than what he have in the acount.
+        matrix_2d[account_number-STARTER][0]-=withdrawal;//init the acount.
         printf("The new balance is: %.2lf\n" , matrix_2d[account_number-STARTER][0]);
-    }
+    }//if the money the user want to withdrawal is bigger than what he have in the acount.
     else printf("Cannot withdraw more than the balance\n");
 
 }
-void C(int account_number){
-     matrix_2d[account_number-STARTER][1]=0;
-     i--;
+void C(int account_number){//this function is to close acount.
+     matrix_2d[account_number-STARTER][1]=0;//close the acount.
+     i--;//update the number of acounts.
 }
-void I(int interest){
+void I(int interest){//this function is to add interest to all the acounts.
     int j;
-    for(j=0;j<ACCOUNTS;j++)
-        if(matrix_2d[j][1]==1)
-            matrix_2d[j][0]=(matrix_2d[j][0]+matrix_2d[j][0]*interest/100);
+    for(j=0;j<ACCOUNTS;j++)//run over the acounts
+        if(matrix_2d[j][1]==1)//if the acount is open,do.
+            matrix_2d[j][0]=(matrix_2d[j][0]+matrix_2d[j][0]*interest/100);//update the acount after the interest.
 }
-void P(){
+void P(){//this function is to print all the open acounts.
     int j=0;
     if(i>0){
-    for(j=0; j<ACCOUNTS;j++){
-        if(matrix_2d[j][1]==1)
+    for(j=0; j<ACCOUNTS;j++){//run over the acounts.
+        if(matrix_2d[j][1]==1)//if the acount is open,print.
             printf("The balance of account number %d is: %.2lf\n" , (STARTER+j), matrix_2d[j][0]);
     }
     }
 }
-void E(){
+void E(){//close all the acounts and get out from this plan.
     int j=0;
-    for(j=0; j<ACCOUNTS;j++){
-        if(matrix_2d[j][1]==1)
+    for(j=0; j<ACCOUNTS;j++){//run over all the acounts.
+        if(matrix_2d[j][1]==1)//if this acount is open,close him.
              matrix_2d[i][1]=0;
     }
 }
-int isopen(int account_number){
+int isopen(int account_number){//function that check if acount is open.
     return matrix_2d[account_number-STARTER][1];
 }
-int isInRange(int account_number){
+int isInRange(int account_number){//function that check if the acount is in the range.
     if(account_number>STARTER-1&&account_number<END)
         return 1;
     else return 0;    
 }
-int getNumberOfOpenAccounts(){
+int getNumberOfOpenAccounts(){//function that return the numbers of the open acount in the bank.
     return i;
 }
